fdf7c9c394d2137bff52ea154c2fc59f03ecc1cc
maheshhattimare <maheshhattimare02@gmail.com>
2024-07-11 16:58:48 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,37 @@ const ninaAssistant = document.getElementById("nina-assistant");
 const ninaHover = document.getElementById("nina-hover");
 const assistantCloseBtn = document.getElementById("assistant-close-btn");
 
-window.document.addEventListener("DOMContentLoaded", () => {
-  ninaAssistant.addEventListener("mouseover", () => {
-    ninaHover.style.display = "block";
-  });
-});
+const smallNavItem1 = document.getElementById("sm-nav-item1");
+const smallNavItemList1 = document.getElementById("sm-nav-item-list1");
 
+const smallNavItem2 = document.getElementById("sm-nav-item2");
+const smallNavItemList2 = document.getElementById("sm-nav-item-list2");
+
+//nina assistant
+window.document.addEventListener("DOMContentLoaded", () => {
+    ninaHover.style.display = "block";
+});
 assistantCloseBtn.addEventListener("click", () => {
   ninaHover.style.display = "none";
 });
+// nina assistant 
+
+// nav items start
+smallNavItem1.addEventListener('mouseenter',()=>{
+  smallNavItemList1.style.display='block';
+});
+smallNavItem1.addEventListener('mouseleave',()=>{
+  smallNavItemList1.style.display='none';
+});
+
+smallNavItem2.addEventListener('mouseenter',()=>{
+  smallNavItemList2.style.display='block';
+});
+smallNavItem2.addEventListener('mouseleave',()=>{
+  smallNavItemList2.style.display='none';
+});
+
+
+
+
+
