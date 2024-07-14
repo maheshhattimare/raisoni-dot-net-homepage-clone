e4f8b857a345f7523f0ec9fedd9fa9c80fa7344d
maheshhattimare <maheshhattimare02@gmail.com>
2024-07-15 02:56:58 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/assets/components/header/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading header:", error));
+
+  fetch("/assets/components/footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading footer:", error));
+  fetch("/assets/components/header/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading footer:", error));
+
+  fetch("/assets/components/header/assistance-and-fab.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("assistance-and-fab").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading footer:", error));
+});
+
+
 const ninaAssistant = document.getElementById("nina-assistant");
 const ninaHover = document.getElementById("nina-hover");
 const assistantCloseBtn = document.getElementById("assistant-close-btn");
 const ninaChatCloseBtn = document.getElementById("nina-chat-close-btn");
-const ninaChatBlock = document.getElementById('nina-chat-block');
-
-//sm-nav
-const smallNavItem1 = document.getElementById("sm-nav-item1");
-const smallNavItemList1 = document.getElementById("sm-nav-item-list1");
-
-const smallNavItem2 = document.getElementById("sm-nav-item2");
-const smallNavItemList2 = document.getElementById("sm-nav-item-list2");
-
-const smallNavItem3 = document.getElementById("sm-nav-item3");
-const smallNavItemList3 = document.getElementById("sm-nav-item-list3");
-
-const smallNavItem4 = document.getElementById("sm-nav-item4");
-const smallNavItemList4 = document.getElementById("sm-nav-item-list4");
-
-const smallNavItem5 = document.getElementById("sm-nav-item5");
-const smallNavItemList5 = document.getElementById("sm-nav-item-list5");
-
-const smallNavItem6 = document.getElementById("sm-nav-item6");
-const smallNavItemList6 = document.getElementById("sm-nav-item-list6");
+const ninaChatBlock = document.getElementById("nina-chat-block");
 
 //nina assistant
 window.document.addEventListener("DOMContentLoaded", () => {
@@ -31,56 +42,11 @@ assistantCloseBtn.addEventListener("click", () => {
   ninaHover.style.display = "none";
 });
 
-ninaAssistant.addEventListener('click',()=>{
-  ninaChatBlock.style.display='block';
-  ninaChatBlock.classList.add('in-amin')
+ninaAssistant.addEventListener("click", () => {
+  ninaChatBlock.style.display = "block";
+  ninaChatBlock.classList.add("in-amin");
 });
-ninaChatCloseBtn.addEventListener('click',()=>{
- ninaChatBlock.style.display='none';
+ninaChatCloseBtn.addEventListener("click", () => {
+  ninaChatBlock.style.display = "none";
 });
-
-
 // nina assistant end
-
-// nav items start
-smallNavItem1.addEventListener('mouseenter',()=>{
-  smallNavItemList1.style.display='block';
-});
-smallNavItem1.addEventListener('mouseleave',()=>{
-  smallNavItemList1.style.display='none';
-});
-
-smallNavItem2.addEventListener('mouseenter',()=>{
-  smallNavItemList2.style.display='block';
-});
-smallNavItem2.addEventListener('mouseleave',()=>{
-  smallNavItemList2.style.display='none';
-});
-
-smallNavItem3.addEventListener('mouseenter',()=>{
-  smallNavItemList3.style.display='block';
-});
-smallNavItem3.addEventListener('mouseleave',()=>{
-  smallNavItemList3.style.display='none';
-});
-
-smallNavItem4.addEventListener('mouseenter',()=>{
-  smallNavItemList4.style.display='block';
-});
-smallNavItem4.addEventListener('mouseleave',()=>{
-  smallNavItemList4.style.display='none';
-});
-
-smallNavItem5.addEventListener('mouseenter',()=>{
-  smallNavItemList5.style.display='block';
-});
-smallNavItem5.addEventListener('mouseleave',()=>{
-  smallNavItemList5.style.display='none';
-});
-
-smallNavItem6.addEventListener('mouseenter',()=>{
-  smallNavItemList6.style.display='block';
-});
-smallNavItem6.addEventListener('mouseleave',()=>{
-  smallNavItemList6.style.display='none';
-});
