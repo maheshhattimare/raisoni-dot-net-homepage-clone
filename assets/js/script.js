2797f11b52093cec501b7c3a902d8e9dc7e801ec
maheshhattimare <maheshhattimare02@gmail.com>
2024-07-15 13:18:28 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("/assets/components/header/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header-placeholder").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading header:", error));
-
-  fetch("/assets/components/footer/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading footer:", error));
-  fetch("/assets/components/header/navbar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("navbar-placeholder").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading footer:", error));
-
-  fetch("/assets/components/header/assistance-and-fab.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("assistance-and-fab").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading footer:", error));
-});
-
-
 const ninaAssistant = document.getElementById("nina-assistant");
 const ninaHover = document.getElementById("nina-hover");
 const assistantCloseBtn = document.getElementById("assistant-close-btn");
